6b7e39f50c9fd649ffb1082bdc445f6634bdbc39
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2021-07-19 09:55:09 +0200

    Definindo Eager Loading para produto listar

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -18,9 +18,9 @@ class ProdutoController extends Controller
     public function index(Request $request)
     {
         // with(['produto_detalhe']) = "eager loading" = carregando produto detalhe junto com a chamada do produto
-        // $produtos = Produto::with(['produto_detalhe'])->paginate(10);
+        $produtos = Produto::with(['produto_detalhe'])->paginate(10);
         // lazy loading = carregamento de produto detalhe somente acontece ao accesarmos o metodo -> produto_detalhe
-        $produtos = Produto::paginate(10);
+        // $produtos = Produto::paginate(10);
         // utilizando model Item que não tem uma padronização como define a conveção do Laravel na nominação das trables e nos seus relacionamentos.
         // $produtos = Item::paginate(10);
         return view('app.produto.listar', compact('produtos', 'request'));
